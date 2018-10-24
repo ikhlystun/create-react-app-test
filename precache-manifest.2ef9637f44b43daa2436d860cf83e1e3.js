5b65a7caf44580e41e4f1b9df50b2c4ed39e9c99
Igor Khlystun <igorhlystun@rambler.ru>
2018-10-24 17:10:46 +0300

    test sharing

---
+++ precache-manifest.2ef9637f44b43daa2436d860cf83e1e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1e9872cfb4cb1d762471",
+    "url": "/static/js/main.1e9872cf.chunk.js"
+  },
+  {
+    "revision": "0f15e672f95ccb64d64c",
+    "url": "/static/js/1.0f15e672.chunk.js"
+  },
+  {
+    "revision": "1e9872cfb4cb1d762471",
+    "url": "/static/css/main.de2d4a49.chunk.css"
+  },
+  {
+    "revision": "a3650603f0551538d402d5c699e34973",
+    "url": "/index.html"
+  }
+];
