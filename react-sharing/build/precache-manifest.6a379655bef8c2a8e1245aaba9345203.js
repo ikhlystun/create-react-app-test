fa95b329c7462bbd08f89444f3b590353ac08dc1
Igor Khlystun <igorhlystun@rambler.ru>
2018-10-24 15:46:55 +0300

    react sharing - pre-alpha

---
+++ react-sharing/build/precache-manifest.6a379655bef8c2a8e1245aaba9345203.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "791d2167c42935f54322",
+    "url": "/static/js/main.791d2167.chunk.js"
+  },
+  {
+    "revision": "0f15e672f95ccb64d64c",
+    "url": "/static/js/1.0f15e672.chunk.js"
+  },
+  {
+    "revision": "791d2167c42935f54322",
+    "url": "/static/css/main.de2d4a49.chunk.css"
+  },
+  {
+    "revision": "7ecf32a63295e7b2f6133f075d8413ed",
+    "url": "/index.html"
+  }
+];
