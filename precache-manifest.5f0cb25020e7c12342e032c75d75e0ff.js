a0078ffb256ad4cdf5ec6759122f9e140fec01ef
Igor Khlystun <igorhlystun@rambler.ru>
2018-10-25 11:16:32 +0300

    123

---
+++ precache-manifest.5f0cb25020e7c12342e032c75d75e0ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2eb3ceac11b57a1d78c0",
+    "url": "/static/js/main.2eb3ceac.chunk.js"
+  },
+  {
+    "revision": "9b8e0e59073de47c81fb",
+    "url": "/static/js/1.9b8e0e59.chunk.js"
+  },
+  {
+    "revision": "2eb3ceac11b57a1d78c0",
+    "url": "/static/css/main.25e04747.chunk.css"
+  },
+  {
+    "revision": "836370d910aae2819168ea8724618bd2",
+    "url": "/index.html"
+  }
+];
