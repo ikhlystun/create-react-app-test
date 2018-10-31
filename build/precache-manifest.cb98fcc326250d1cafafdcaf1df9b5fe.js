177882e1ad831d6344c7659506769fc80d1f767b
Igor Khlystun <igorhlystun@rambler.ru>
2018-10-31 15:49:26 +0200

    v.781

---
+++ build/precache-manifest.cb98fcc326250d1cafafdcaf1df9b5fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8caa15b8a896d9bc438e4bf38fae208d",
+    "url": "/build/static/media/logo.8caa15b8.jpg"
+  },
+  {
+    "revision": "3923d73d01a4271659cc",
+    "url": "/build/static/js/runtime~main.3923d73d.js"
+  },
+  {
+    "revision": "0f736640b718ff030dd3",
+    "url": "/build/static/js/main.0f736640.chunk.js"
+  },
+  {
+    "revision": "2b60108874d7382021a0",
+    "url": "/build/static/js/1.2b601088.chunk.js"
+  },
+  {
+    "revision": "0f736640b718ff030dd3",
+    "url": "/build/static/css/main.b472548a.chunk.css"
+  },
+  {
+    "revision": "b601eb2417742e43fc356ce3a23c605a",
+    "url": "/build/index.html"
+  }
+];
